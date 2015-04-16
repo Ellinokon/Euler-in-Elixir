9c4e07db5a4b292c785f98f50a4d81772fd976cf
Håkon Ellingsen <hakonweb@gmail.com>
2015-04-16 10:18:46 +0200

    Use module `Integer` instead of custom function

--- lib/euler/problem2.ex
+++ lib/euler/problem2.ex
@@ -1,10 +1,12 @@
 defmodule Euler.Problem2 do
+  import Integer, only: :macros
+
   def answer, do: solve(4000000)
 
   def solve(limit) when is_integer(limit) do
     fib_stream()
     |> Enum.take_while(&(&1 <= limit))
-    |> Enum.filter(&odd?/1)
+    |> Enum.filter(&is_odd/1)
     |> Enum.sum
   end
 
@@ -13,11 +15,4 @@ defmodule Euler.Problem2 do
   defp fib(0), do: 0
   defp fib(1), do: 1
   defp fib(n) when n > 1, do: fib(n - 1) + fib(n - 2)
-
-  defp odd?(num) do
-    case num do
-      x when rem(x, 2) == 1 -> true
-      _                     -> false
-    end
-  end
 end
